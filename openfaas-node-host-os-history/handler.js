01058293e8afdaa69900918095c54905742d8724
yonghui <yonghui.park@kt.com>
2020-02-16 20:59:38 +0900

    initial commit

--- openfaas-node-host-os-history/handler.js
+++ openfaas-node-host-os-history/handler.js
@@ -2,18 +2,18 @@
 
 const { Client }  = require('pg')
 const client = new Client({
-  user: 'icmdb',
-  host: '211.251.237.70',
-  database: 'icm',
-  password: 'icmdb',
-  port: 15432
+  user: 'postgres',
+  host: '192.168.43.70',
+  database: 'postgres',
+  password: 'postgres',
+  port: 5432
 })
 client.connect()
 
 module.exports = (event, context) => {
     let err;
     console.log('start server =====> query selec ' )
-    client.query('SELECT * FROM icmdb.tb_host_os_patch_history', (err, res) => {
+    client.query('SELECT * FROM public.tb_host_os_patch_history', (err, res) => {
       console.log(err,res)
       context.status(200).succeed(res.rows);
     })
